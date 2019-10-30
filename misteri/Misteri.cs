760b0c530b27f5bff6ebef0192bf0badf202c370
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-30 12:09:14 +0100

    Add project files.

---
+++ misteri/Misteri.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mistero1
+{
+    class Misteri
+    {
+        public static int mistero1(int x, int y)
+        {
+            int prec;
+            if (y == 0)
+            {
+                return x;
+            }
+            else
+            {
+                prec = mistero1(x, y - 1) + 1;
+                return prec;
+            }
+        }
+        public static int mistero2(int x, int y)
+        {
+            if (y == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (mistero1(x, mistero2(x, y - 1)));
+            }
+        }
+        public static int mistero3(int x, int y)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (mistero2(x, mistero3(x, y - 1)));
+            }
+        }
+        // si tratta di divisione 
+        public static string Mistero4(long n)
+        {
+            Console.Write(n % 10);
+            if (n / 10 != 0)
+                return Mistero4(n / 10);
+            else
+                return "";
+        }
+        public static int Mistero5(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            return (Mistero5(n - 1) + Mistero5(n - 2));
+        }
+        public static int Mistero6(int x, int y)
+        {
+            if (x < 0)
+                return Mistero6(x + y, y);
+            else if (x < y)
+                return x;
+            else
+                return Mistero6(x - y, y);
+        }
+        public static int Mistero7(int x, int y)
+        {
+            int z = 1;
+            if (y < 0)
+                z = 0;
+            else if (y > 0)
+                z = x * Mistero7(x, y - 1);
+            return z;
+        }
+        public static int Mistero8(int a, int b)
+        {
+            int z;
+            z = a % b;
+            if (z == 0)
+                return b;
+            else
+                a = b;
+            b = z;
+            return Mistero8(a, b);
+        }
+    }
+}
